622177ddbf096bb46a400a6d6c30edef6f7be21c
Vinicius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-04-27 11:54:57 -0300

    Adicionado método de pesquisa

--- mural-frontend/client.js
+++ mural-frontend/client.js
@@ -1,46 +1,56 @@
 const listagem_view = document.getElementById('listagem');
-const title = document.getElementById('title');
-const msg = document.getElementById('msg');
-const author = document.getElementById('author');
-
 const mensagens = [];
 
-fetch('http://150.165.85.16:9900/api/msgs')
-.then(r => r.json())
-.then(data => {
-    Object.assign(mensagens, data);
-    update_view();
-});
+function update_msg() {
+    fetch('http://150.165.85.16:9900/api/msgs')
+    .then(r => r.json())
+    .then(data => { 
+        Object.assign(mensagens, data);
+        update_view(mensagens.reverse())
+    }
+    );
+}
 
-function update_view() {
-    const items = mensagens.map(e => `
-    <div class="card shadow">
-        <div class="card-body">
-            <h5 class="card-title text-primary">${e.title}</h5> 
-            <p class="card-text">${e.msg}</p> 
-            <p class="card-text">
-                <small class="text-muted">@${e.author}</small>
-                <small class="text-muted font-italic"> --- ${e.created_at}</small>
-            </p>
+function update_view(array) {
+        const items = array.map(e => `
+        <div class="card shadow">
+            <div class="card-body">
+                <h5 class="card-title text-primary">${e.title}</h5> 
+                <p class="card-text">${e.msg}</p> 
+                <p class="card-text">
+                    <small class="text-muted">@${e.author}</small>
+                    <small class="text-muted font-italic"> --- ${e.created_at}</small>
+                </p>
         </div>
     </div>
     <br>`).join("\n");
-    listagem_view.innerHTML = items;
+    listagem_view.innerHTML = items; 
 };
 
 function submit() {
     fetch('http://150.165.85.16:9900/api/msgs', {
         method: 'POST',
         body: JSON.stringify({
-            title: title.value, 
-            msg: msg.value, 
-            author: author.value, 
+            title: document.getElementById('subject').value,
+            msg: document.getElementById('msg').value, 
+            author: document.getElementById('author').value, 
             credentials:"vjsilva:avengers"})
     })
     .then(dado => dado.json());
-    // window.location.reload();
+    update_msg();
 };
 
-function show() {
-    document.getElementById('add').hidden = !document.getElementById('add').hidden;
+function show(tag) {
+    tag.hidden = !tag.hidden;
+}
+
+function clearvalue(tag) {
+    tag.value = '';
+}
+
+update_msg();
+
+function searching(tag) {
+    const busca = mensagens.filter(a => a.title.toLowerCase().indexOf(tag.value.toLowerCase()) != -1);
+    update_view(busca);
 }
