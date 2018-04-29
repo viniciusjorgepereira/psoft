701894656f7860c9ad8e7886106926258fb4abb2
Vinícius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-04-29 13:22:23 -0300

    Implementado a função de remoção e melhorias

--- mural-frontend/client.js
+++ mural-frontend/client.js
@@ -6,21 +6,24 @@ function update_msg() {
     .then(r => r.json())
     .then(data => { 
         Object.assign(mensagens, data);
-        update_view(mensagens.reverse())
-    }
-    );
+        update_view(mensagens);
+    });
 }
 
 function update_view(array) {
-        const items = array.map(e => `
-        <div class="card shadow">
-            <div class="card-body">
-                <h5 class="card-title text-primary">${e.title}</h5> 
-                <p class="card-text">${e.msg}</p> 
-                <p class="card-text">
-                    <small class="text-muted">@${e.author}</small>
-                    <small class="text-muted font-italic"> --- ${e.created_at}</small>
-                </p>
+    const items = array.map(e => `
+    <div class="card shadow mx-2 my-2 w-auto">
+        <div class="card-body">
+            <span title="Identificador da mensagem" class="badge badge-primary badge-pill float-right">#${e.id}</span>
+            <h5 class="card-title text-primary">${e.title}</h5> 
+            <p class="card-text">${e.msg}</p> 
+            <p class="card-text">
+                <small class="text-muted">@${e.author}</small>
+                <small class="text-muted font-italic">
+                    , enviado às ${new Date(e.created_at).toLocaleTimeString()}
+                    , em ${new Date(e.created_at).toLocaleDateString()} 
+                </small>
+            </p>
         </div>
     </div>
     <br>`).join("\n");
@@ -51,6 +54,36 @@ function clearvalue(tag) {
 update_msg();
 
 function searching(tag) {
-    const busca = mensagens.filter(a => a.title.toLowerCase().indexOf(tag.value.toLowerCase()) != -1);
-    update_view(busca);
+    const busca = mensagens.filter(a => 
+        a.title.toLowerCase().indexOf(tag.value.toLowerCase()) != -1 ||
+        a.msg.toLowerCase().indexOf(tag.value.toLowerCase()) != -1 ||
+        a.author.toLowerCase().indexOf(tag.value.toLowerCase()) != -1
+    );
+    if (busca.length == 0) {
+        alert('Nenhuma mensagem correspondente foi encontrada');
+    } else {
+        update_view(busca);
+    }
+}
+
+function localmessages() {
+    return mensagens.filter(a => 
+        a.frontend == 'vjsilva'
+    );
+}
+
+function remove (tag) {
+    if (tag.value.length == 0) {
+        alert('Informe um id');
+    } else {   
+        const msgs = JSON.stringify({
+            credentials: "vjsilva:avengers"
+        });
+        fetch(`http://150.165.85.16:9900/api/msgs/${tag.value}`, {
+            method: 'DELETE',
+            body: msgs
+        })
+        .then(a => localmessages);
+        update_msg();
+    }
 }
